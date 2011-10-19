70aee5943db898ba607464542ccc6a004251075c
Ophir Lifshitz <hangfromthefloor@gmail.com>
2011-10-19 17:19:18 +0000

    Added capability for selecting year and term in example API entry form

--- umd-examples.php
+++ umd-examples.php
@@ -1,8 +1,10 @@
 <?php
 
 echo '<form method="get" action="' . $_SERVER['PHP_SELF'] . '">
+<label for="year">Year:</label> <select id="year" name="year"><option value="2011" selected="selected">2011</option><option value="2012">2012</option></select>
+<label for="term">Term:</label> <select id="term" name="term"><option value="08" selected="selected" style="font-weight: bold;">Fall</option><option value="12">Winter</option><option value="01" style="font-weight: bold;">Spring</option><option value="06">Summer</option></select>
 <label for="dept">Department or course:</label> <input type="text" id="dept" name="dept" value="' . @$_GET['dept'] . '" />
-<label for="dept">Section:</label> <input type="text" id="sec" name="sec" value="' . @$_GET['sec'] . '" />
+<label for="sec">Section:</label> <input type="text" id="sec" name="sec" value="' . @$_GET['sec'] . '" />
 <input type="submit" value="Submit" /></form>';
 
 
@@ -23,7 +25,7 @@ foreach ($departments as $department)
 //$courses = $umdapi->get_schedule('2010','08','hebr');
 
 //Get Course Schedule for current term
-$courses = $umdapi->get_schedule(null,null,@$_GET['dept'],@$_GET['sec']);
+$courses = $umdapi->get_schedule(@$_GET['year'],@$_GET['term'],@$_GET['dept'],@$_GET['sec']);
 echo'<pre>';print_r($courses);echo'</pre>';
 
 /*
